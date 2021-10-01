01a6eb7cbf281822607cee0102d97fb90e8aec34
schi48 <91401835+schi48@users.noreply.github.com>
2021-10-01 01:19:56 -0400

    Add files via upload

---
+++ toh-pt2/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1/11/21, name: 'Sarah Miller' },
+  { id: 1/11/21, name: 'Leo Tsukinaga' },
+  { id: 1/12/21, name: 'Stacy Chi' },
+  { id: 1/13/21, name: 'Jessica Kim' },
+  { id: 1/13/21, name: 'Bob White' },
+  { id: 1/14/21, name: 'Jaime Gomez' },
+  { id: 1/14/21, name: 'Mary Lee' },
+  { id: 1/14/21, name: 'Linda Bethany' },
+  { id: 1/15/21, name: 'Nyan Cat' },
+  { id: 1/16/21, name: 'Voldemort' }
+];
